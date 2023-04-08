3a1f5c33a0e44eb5e98abe3b7899fbc368deac58
Ramon Brito <ramon.mecatec@gmail.com>
2023-04-07 22:34:45 -0300

    Make all new user email confirmed

--- app/models.py
+++ app/models.py
@@ -29,7 +29,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-    confirmed = db.Column(db.Boolean, default=False)
+    confirmed = db.Column(db.Boolean, default=True)
 
     @property
     def password(self):
