1ca8a464407dc96edb6231623ad8f96183d65dcb
Ramon Brito <ramon.mecatec@gmail.com>
2023-05-07 14:06:28 -0300

    Insert user profile information into User model

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ from . import login_manager
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
 from . import db
+from datetime import datetime
 
 
 @login_manager.user_loader
@@ -79,6 +80,12 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     confirmed = db.Column(db.Boolean, default=True)
+
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text)
+    member_since = db.Column(db.DateTime(), default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
 
     def __init__(self, **kwargs) -> None:
         super(User, self).__init__(**kwargs)
